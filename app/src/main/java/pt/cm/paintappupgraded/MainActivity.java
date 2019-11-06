dc09ac9ca68d36fc548727a45f486f8389e4a6cf
arbsantos <andre13521@gmail.com>
2019-11-06 11:31:46 +0000

    Initial commit

---
+++ app/src/main/java/pt/cm/paintappupgraded/MainActivity.java
@@ -0,0 +1,26 @@
+package pt.cm.paintappupgraded;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        GestureListener mGestureListener = new GestureListener();
+        GestureDetector mGestureDetector = new GestureDetector(getApplicationContext(), mGestureListener);
+        mGestureDetector.setIsLongpressEnabled(true);
+        mGestureDetector.setOnDoubleTapListener(mGestureListener);
+
+        PaintCanvas paintCanvas = new PaintCanvas(getApplicationContext(), null, mGestureDetector);
+        mGestureListener.setCanvas(paintCanvas);
+
+        setContentView(paintCanvas);// adds the created view to the screen
+    }
+
+}
